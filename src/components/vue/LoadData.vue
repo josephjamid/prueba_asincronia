1c19c4a3a095e7cfb8bee7497f41e5352a2a8df8
josephjamid <josephjamid@gmail.com>
2024-06-07 18:03:11 -0500

    pruebaasincronia

---
+++ src/components/vue/LoadData.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {ref} from 'vue'
+
+const users = ref([])
+
+
+
+
+function LoadData (){
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(function(response)  {
+            return response.json()
+        })
+        .then(function (data){
+            console.log(data);
+            users.value = data
+        })
+   
+
+
+}
+
+
+</script>
+
+<template >
+
+    <div class="bg-slate-300 mb-1 mt-4  " v-for="user in users " :key="users.id">{{ user.name }}</div>
+    <button class="bg-red-400 mx-auto flex"  @click="LoadData">
+        cargar info
+    </button>
+      
+    
+</template>
